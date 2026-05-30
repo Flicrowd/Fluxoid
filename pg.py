97cb05c32a46de506c9e7190bb816950fd02eae5
Flicrowd <alek.kupryushin@yandex.ru>
2026-05-30 15:40:57 +0300

    Update pg.py

--- pg.py
+++ pg.py
@@ -6,7 +6,7 @@ pygame.mixer.init()
 WIDTH = 1060
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Арканоид — Космический Проект")
+pygame.display.set_caption("Fluxoid")
 
 pygame.mixer.music.load("background.mp3")
 pygame.mixer.music.play(-1) 
