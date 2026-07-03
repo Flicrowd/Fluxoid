c8107e2dd38f7a679c759430b6f25986d785b019
Flicrowd <akuprusin801@gmail.com>
2026-07-03 09:25:50 +0300

    5
    Prepare for v1.2.0

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ bg_img = pygame.image.load(get_path("background.png"))
 icon = pygame.image.load(get_path("Fluxoid_Logo.png"))
 pygame.display.set_icon(icon)
 
+# Загружаем логотип 512x512 и плавно сжимаем его до 200x200 для меню
+menu_logo = pygame.image.load(get_path("Fluxoid_Logo.png"))
+menu_logo = pygame.transform.scale(menu_logo, (200, 200))
+
 game_over_sound = pygame.mixer.Sound(get_path("game_over.wav"))
 win_sound = pygame.mixer.Sound(get_path("win.wav"))
 
@@ -47,6 +51,7 @@ font = pygame.font.SysFont("Arial", 36)
 retry_font = pygame.font.SysFont("Arial", 28)
 final_font = pygame.font.SysFont("Arial", 72)
 music_font = pygame.font.SysFont("Arial", 24)
+menu_title_font = pygame.font.SysFont("Arial", 80, bold=True)
 
 paddle_width = 120
 paddle_height = 15
@@ -67,17 +72,26 @@ ball_dx = 5
 ball_dy = -5
 
 bricks = []
-for row in range(5):        
-    for col in range(11):   
-        bx = 110 + col * 75 
-        by = 60 + row * 30  
-        brick = pygame.Rect(bx, by, 70, 25)
-        bricks.append(brick)
+def reset_bricks():
+    global bricks
+    bricks = []
+    for row in range(5):        
+        for col in range(11):   
+            bx = 110 + col * 75 
+            by = 60 + row * 30  
+            brick = pygame.Rect(bx, by, 70, 25)
+            bricks.append(brick)
+
+reset_bricks()
 
 score = 0
 game_over = False
 win = False
 running = True 
+
+game_state = "MENU"
+menu_options = ["START GAME", "SELECT MUSIC", "EXIT"]
+current_menu_index = 0
 
 while running:
     screen.blit(bg_img, (0, 0))
@@ -85,113 +99,162 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False  
+        
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                move_left = True
-            elif event.key == pygame.K_RIGHT:
-                move_right = True
-            elif event.key == pygame.K_1:
-                current_track_index = 0
-                play_selected_track()
-            elif event.key == pygame.K_2:
-                current_track_index = 1
-                play_selected_track()
-            elif event.key == pygame.K_RETURN and (game_over == True or win == True):
-                paddle.x = 470
-                ball.x = 530
-                ball.y = 300
-                ball_dx = 5
-                ball_dy = -5
-                score = 0
-                game_over = False
-                win = False
-                move_left = False
-                move_right = False
-                bricks = []
-                for row in range(5):        
-                    for col in range(11):   
-                        bx = 110 + col * 75 
-                        by = 60 + row * 30  
-                        brick = pygame.Rect(bx, by, 70, 25)
-                        bricks.append(brick)
-                play_selected_track()
+            if game_state == "MENU":
+                if event.key == pygame.K_UP:
+                    current_menu_index = (current_menu_index - 1) % len(menu_options)
+                elif event.key == pygame.K_DOWN:
+                    current_menu_index = (current_menu_index + 1) % len(menu_options)
+                elif event.key == pygame.K_RETURN:
+                    if current_menu_index == 0:
+                        game_state = "PLAY"
+                        paddle.x = 470
+                        ball.x = 530
+                        ball.y = 300
+                        ball_dx = 5
+                        ball_dy = -5
+                        score = 0
+                        game_over = False
+                        win = False
+                        reset_bricks()
+                        play_selected_track()
+                    elif current_menu_index == 1:
+                        game_state = "MUSIC_SELECT"
+                    elif current_menu_index == 2:
+                        running = False
+
+            elif game_state == "MUSIC_SELECT":
+                if event.key == pygame.K_1:
+                    current_track_index = 0
+                    play_selected_track()
+                elif event.key == pygame.K_2:
+                    current_track_index = 1
+                    play_selected_track()
+                elif event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+                    game_state = "MENU"
+
+            elif game_state == "PLAY":
+                if event.key == pygame.K_LEFT:
+                    move_left = True
+                elif event.key == pygame.K_RIGHT:
+                    move_right = True
+                elif event.key == pygame.K_ESCAPE:
+                    game_state = "MENU"
+                elif event.key == pygame.K_RETURN and (game_over == True or win == True):
+                    game_state = "MENU"
+                elif (game_over == True or win == True):
+                    if event.key == pygame.K_1:
+                        current_track_index = 0
+                        play_selected_track()
+                    elif event.key == pygame.K_2:
+                        current_track_index = 1
+                        play_selected_track()
+
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                move_left = False
-            elif event.key == pygame.K_RIGHT:
-                move_right = False
+            if game_state == "PLAY":
+                if event.key == pygame.K_LEFT:
+                    move_left = False
+                elif event.key == pygame.K_RIGHT:
+                    move_right = False
 
-    if game_over == False and win == False:
-        if move_left == True and paddle.x > 0:
-            paddle.x -= paddle_speed
-        if move_right == True and paddle.x < WIDTH - paddle_width:
-            paddle.x += paddle_speed
-
-        ball.x += ball_dx
-        ball.y += ball_dy
-
-        if ball.x <= 0 or ball.x >= WIDTH - ball_size:
-            ball_dx = -ball_dx
+    if game_state == "MENU":
+        logo_x = (WIDTH - 200) // 2
+        screen.blit(menu_logo, (logo_x, 30))
         
-        if ball.y <= 0:
-            ball_dy = -ball_dy
-
-        if ball.y >= HEIGHT:
-            game_over = True
-            pygame.mixer.music.stop() 
-            game_over_sound.play()
-
-        if ball.colliderect(paddle):
-            ball_dy = -ball_dy
-
-        for brick in bricks:
-            if ball.colliderect(brick):
-                bricks.remove(brick)  
-                ball_dy = -ball_dy    
-                score += 10           
-                break  
-
-        if len(bricks) == 0:
-            win = True
-            pygame.mixer.music.stop() 
-            win_sound.play()
-
-        pygame.draw.rect(screen, NEON_CYAN, paddle)
-        pygame.draw.circle(screen, WHITE, ball.center, 8)
-
-        for brick in bricks:
-            pygame.draw.rect(screen, NEON_ORANGE, brick)
-
-        score_text = font.render("Score: " + str(score), True, YELLOW)
-        screen.blit(score_text, (20, 15))
-
-    if game_over == True:
-        loss_text = final_font.render("GAME OVER", True, RED)
-        screen.blit(loss_text, (350, 200))
-        retryo_text = retry_font.render("To play again press ENTER", True, WHITE)
-        screen.blit(retryo_text, (380, 420))
+        title_text = menu_title_font.render("FLUXOID", True, NEON_CYAN)
+        screen.blit(title_text, ((WIDTH - title_text.get_width()) // 2, 240))
         
-        current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
-        music_x = (WIDTH - current_music_text.get_width()) // 2
-        screen.blit(current_music_text, (music_x, 470))
-        
-        hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
-        hint_x = (WIDTH - hint_text.get_width()) // 2
-        screen.blit(hint_text, (hint_x, 510))
+        for i, option in enumerate(menu_options):
+            if i == current_menu_index:
+                opt_text = font.render("> " + option + " <", True, NEON_ORANGE)
+            else:
+                opt_text = font.render(option, True, WHITE)
+            screen.blit(opt_text, ((WIDTH - opt_text.get_width()) // 2, 360 + i * 55))
+            
+        info_text = music_font.render("Use UP / DOWN arrows and ENTER to select", True, GRAY)
+        screen.blit(info_text, ((WIDTH - info_text.get_width()) // 2, 540))
 
-    if win == True:
-        win_text = final_font.render("YOU WIN", True, GREEN)
-        screen.blit(win_text, (390, 200))
-        retryw_text = retry_font.render("To play again press ENTER", True, WHITE)
-        screen.blit(retryw_text, (380, 420))
+    elif game_state == "MUSIC_SELECT":
+        title_text = font.render("SELECT BACKGROUND MUSIC", True, NEON_CYAN)
+        screen.blit(title_text, ((WIDTH - title_text.get_width()) // 2, 100))
         
-        current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
-        music_x = (WIDTH - current_music_text.get_width()) // 2
-        screen.blit(current_music_text, (music_x, 470))
-        
-        hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
-        hint_x = (WIDTH - hint_text.get_width()) // 2
-        screen.blit(hint_text, (hint_x, 510))
+        for i, track in enumerate(tracks):
+            if i == current_track_index:
+                t_text = font.render(f"[{i+1}] " + track["name"] + " (Active)", True, NEON_ORANGE)
+            else:
+                t_text = font.render(f"[{i+1}] " + track["name"], True, WHITE)
+            screen.blit(t_text, ((WIDTH - t_text.get_width()) // 2, 220 + i * 60))
+            
+        back_text = music_font.render("Press 1 or 2 to listen. Press ESC / ENTER to return to Menu", True, YELLOW)
+        screen.blit(back_text, ((WIDTH - back_text.get_width()) // 2, 450))
+
+    elif game_state == "PLAY":
+        if game_over == False and win == False:
+            if move_left == True and paddle.x > 0:
+                paddle.x -= paddle_speed
+            if move_right == True and paddle.x < WIDTH - paddle_width:
+                paddle.x += paddle_speed
+
+            ball.x += ball_dx
+            ball.y += ball_dy
+
+            if ball.x <= 0 or ball.x >= WIDTH - ball_size:
+                ball_dx = -ball_dx
+            
+            if ball.y <= 0:
+                ball_dy = -ball_dy
+
+            if ball.y >= HEIGHT:
+                game_over = True
+                pygame.mixer.music.stop() 
+                game_over_sound.play()
+
+            if ball.colliderect(paddle):
+                ball_dy = -ball_dy
+
+            for brick in bricks:
+                if ball.colliderect(brick):
+                    bricks.remove(brick)  
+                    ball_dy = -ball_dy    
+                    score += 10           
+                    break  
+
+            if len(bricks) == 0:
+                win = True
+                pygame.mixer.music.stop() 
+                win_sound.play()
+
+            pygame.draw.rect(screen, NEON_CYAN, paddle)
+            pygame.draw.circle(screen, WHITE, ball.center, 8)
+
+            for brick in bricks:
+                pygame.draw.rect(screen, NEON_ORANGE, brick)
+
+            score_text = font.render("Score: " + str(score), True, YELLOW)
+            screen.blit(score_text, (20, 15))
+
+        if game_over == True:
+            loss_text = final_font.render("GAME OVER", True, RED)
+            screen.blit(loss_text, ((WIDTH - loss_text.get_width()) // 2, 160))
+            retryo_text = retry_font.render("Press ENTER to return to Menu", True, WHITE)
+            screen.blit(retryo_text, ((WIDTH - retryo_text.get_width()) // 2, 380))
+            
+            current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
+            screen.blit(current_music_text, ((WIDTH - current_music_text.get_width()) // 2, 450))
+            hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
+            screen.blit(hint_text, ((WIDTH - hint_text.get_width()) // 2, 490))
+
+        if win == True:
+            win_text = final_font.render("YOU WIN", True, GREEN)
+            screen.blit(win_text, ((WIDTH - win_text.get_width()) // 2, 160))
+            retryw_text = retry_font.render("Press ENTER to return to Menu", True, WHITE)
+            screen.blit(retryw_text, ((WIDTH - retryw_text.get_width()) // 2, 380))
+            
+            current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
+            screen.blit(current_music_text, ((WIDTH - current_music_text.get_width()) // 2, 450))
+            hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
+            screen.blit(hint_text, ((WIDTH - hint_text.get_width()) // 2, 490))
 
     pygame.display.flip()
     clock.tick(FPS)
--- main_wo_sys.py
+++ main_wo_sys.py
@@ -1,195 +1,138 @@
 import pygame
-
 pygame.init()
-pygame.mixer.init() 
-
-WIDTH = 1060
-HEIGHT = 600
+pygame.mixer.init()
+WIDTH, HEIGHT = 1060, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Fluxoid")
 
 tracks = [
     {"file": "background.mp3", "name": "One Sly Move"},
-    {"file": "track2.mp3", "name": "Cloud Dancer"}
+    {"file": "track2.mp3", "name": "Cloud Dancer"},
+    {"file": "track3.mp3", "name": "Galactic Rap"},
+    {"file": "track4.mp3", "name": "Mesmerizing Galaxy Loop"}
 ]
 current_track_index = 0
 
 def play_selected_track():
     pygame.mixer.music.load(tracks[current_track_index]["file"])
-    pygame.mixer.music.play(-1) 
+    pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.4)
 
 play_selected_track()
-
 bg_img = pygame.image.load("background.png")
-icon = pygame.image.load("Fluxoid_Logo.png")
-pygame.display.set_icon(icon)
-
+pygame.display.set_icon(pygame.image.load("Fluxoid_Logo.png"))
+menu_logo = pygame.transform.scale(pygame.image.load("Fluxoid_Logo.png"), (200, 200))
 game_over_sound = pygame.mixer.Sound("game_over.wav")
 win_sound = pygame.mixer.Sound("win.wav")
 
-WHITE = (255, 255, 255)
-NEON_CYAN = (0, 200, 255)
-NEON_ORANGE = (255, 120, 0)
-YELLOW = (255, 215, 0)
-RED = (230, 50, 50)
-GREEN = (50, 230, 50)
-GRAY = (150, 150, 150)
-
-clock = pygame.time.Clock()
-FPS = 60
-
+WHITE, NEON_CYAN, NEON_ORANGE, YELLOW, RED, GREEN, GRAY = (255,255,255), (0,200,255), (255,120,0), (255,215,0), (230,50,50), (50,230,50), (150,150,150)
+clock, FPS = pygame.time.Clock(), 60
 font = pygame.font.SysFont("Arial", 36)
 retry_font = pygame.font.SysFont("Arial", 28)
 final_font = pygame.font.SysFont("Arial", 72)
 music_font = pygame.font.SysFont("Arial", 24)
+menu_title_font = pygame.font.SysFont("Arial", 80, bold=True)
 
-paddle_width = 120
-paddle_height = 15
-paddle_x = 470  
-paddle_y = 550
-paddle_speed = 8
-paddle = pygame.Rect(paddle_x, paddle_y, paddle_width, paddle_height)
-
-move_left = False
-move_right = False
-
-ball_size = 16
-ball_x = 530     
-ball_y = 300
-ball = pygame.Rect(ball_x, ball_y, ball_size, ball_size)
-
-ball_dx = 5
-ball_dy = -5
-
+paddle = pygame.Rect(470, 550, 120, 15)
+move_left, move_right = False, False
+ball = pygame.Rect(530, 300, 16, 16)
+ball_dx, ball_dy = 5, -5
 bricks = []
-for row in range(5):        
-    for col in range(11):   
-        bx = 110 + col * 75 
-        by = 60 + row * 30  
-        brick = pygame.Rect(bx, by, 70, 25)
-        bricks.append(brick)
 
-score = 0
-game_over = False
-win = False
-running = True 
+def reset_bricks():
+    global bricks
+    bricks = [pygame.Rect(110 + col * 75, 60 + row * 30, 70, 25) for row in range(5) for col in range(11)]
+
+reset_bricks()
+score, game_over, win, running = 0, False, False, True
+game_state, menu_options, current_menu_index = "MENU", ["START GAME", "SELECT MUSIC", "EXIT"], 0
 
 while running:
     screen.blit(bg_img, (0, 0))
-
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False  
+        if event.type == pygame.QUIT: running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                move_left = True
-            elif event.key == pygame.K_RIGHT:
-                move_right = True
-            elif event.key == pygame.K_1:
-                current_track_index = 0
-                play_selected_track()
-            elif event.key == pygame.K_2:
-                current_track_index = 1
-                play_selected_track()
-            elif event.key == pygame.K_RETURN and (game_over == True or win == True):
-                paddle.x = 470
-                ball.x = 530
-                ball.y = 300
-                ball_dx = 5
-                ball_dy = -5
-                score = 0
-                game_over = False
-                win = False
-                move_left = False
-                move_right = False
-                bricks = []
-                for row in range(5):        
-                    for col in range(11):   
-                        bx = 110 + col * 75 
-                        by = 60 + row * 30  
-                        brick = pygame.Rect(bx, by, 70, 25)
-                        bricks.append(brick)
-                play_selected_track()
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                move_left = False
-            elif event.key == pygame.K_RIGHT:
-                move_right = False
+            if game_state == "MENU":
+                if event.key == pygame.K_UP: current_menu_index = (current_menu_index - 1) % len(menu_options)
+                elif event.key == pygame.K_DOWN: current_menu_index = (current_menu_index + 1) % len(menu_options)
+                elif event.key == pygame.K_RETURN:
+                    if current_menu_index == 0:
+                        game_state, score, game_over, win, move_left, move_right = "PLAY", 0, False, False, False, False
+                        paddle.x, ball.x, ball.y, ball_dx, ball_dy = 470, 530, 300, 5, -5
+                        reset_bricks()
+                        play_selected_track()
+                    elif current_menu_index == 1: game_state = "MUSIC_SELECT"
+                    elif current_menu_index == 2: running = False
+            elif game_state == "MUSIC_SELECT":
+                if event.key in [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4]:
+                    current_track_index = event.key - pygame.K_1
+                    play_selected_track()
+                elif event.key in [pygame.K_ESCAPE, pygame.K_RETURN]: game_state = "MENU"
+            elif game_state == "PLAY":
+                if event.key == pygame.K_LEFT: move_left = True
+                elif event.key == pygame.K_RIGHT: move_right = True
+                elif event.key == pygame.K_ESCAPE: game_state = "MENU"
+                elif event.key == pygame.K_RETURN and (game_over or win): game_state = "MENU"
+                elif (game_over or win) and event.key in [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4]:
+                    current_track_index = event.key - pygame.K_1
+                    play_selected_track()
+        elif event.type == pygame.KEYUP and game_state == "PLAY":
+            if event.key == pygame.K_LEFT: move_left = False
+            elif event.key == pygame.K_RIGHT: move_right = False
 
-    if game_over == False and win == False:
-        if move_left == True and paddle.x > 0:
-            paddle.x -= paddle_speed
-        if move_right == True and paddle.x < WIDTH - paddle_width:
-            paddle.x += paddle_speed
-
-        ball.x += ball_dx
-        ball.y += ball_dy
-
-        if ball.x <= 0 or ball.x >= WIDTH - ball_size:
-            ball_dx = -ball_dx
-        
-        if ball.y <= 0:
-            ball_dy = -ball_dy
-
-        if ball.y >= HEIGHT:
-            game_over = True
-            pygame.mixer.music.stop() 
-            game_over_sound.play()
-
-        if ball.colliderect(paddle):
-            ball_dy = -ball_dy
-
-        for brick in bricks:
-            if ball.colliderect(brick):
-                bricks.remove(brick)  
-                ball_dy = -ball_dy    
-                score += 10           
-                break  
-
-        if len(bricks) == 0:
-            win = True
-            pygame.mixer.music.stop() 
-            win_sound.play()
-
-        pygame.draw.rect(screen, NEON_CYAN, paddle)
-        pygame.draw.circle(screen, WHITE, ball.center, 8)
-
-        for brick in bricks:
-            pygame.draw.rect(screen, NEON_ORANGE, brick)
-
-        score_text = font.render("Score: " + str(score), True, YELLOW)
-        screen.blit(score_text, (20, 15))
-
-    if game_over == True:
-        loss_text = final_font.render("GAME OVER", True, RED)
-        screen.blit(loss_text, (350, 200))
-        retryo_text = retry_font.render("To play again press ENTER", True, WHITE)
-        screen.blit(retryo_text, (380, 420))
-        
-        current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
-        music_x = (WIDTH - current_music_text.get_width()) // 2
-        screen.blit(current_music_text, (music_x, 470))
-        
-        hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
-        hint_x = (WIDTH - hint_text.get_width()) // 2
-        screen.blit(hint_text, (hint_x, 510))
-
-    if win == True:
-        win_text = final_font.render("YOU WIN", True, GREEN)
-        screen.blit(win_text, (390, 200))
-        retryw_text = retry_font.render("To play again press ENTER", True, WHITE)
-        screen.blit(retryw_text, (380, 420))
-        
-        current_music_text = music_font.render("Current Music: " + tracks[current_track_index]["name"], True, GRAY)
-        music_x = (WIDTH - current_music_text.get_width()) // 2
-        screen.blit(current_music_text, (music_x, 470))
-        
-        hint_text = music_font.render("Press 1 or 2 to change music track", True, YELLOW)
-        hint_x = (WIDTH - hint_text.get_width()) // 2
-        screen.blit(hint_text, (hint_x, 510))
-
+    if game_state == "MENU":
+        screen.blit(menu_logo, ((WIDTH - 200) // 2, 30))
+        t_text = menu_title_font.render("FLUXOID", True, NEON_CYAN)
+        screen.blit(t_text, ((WIDTH - t_text.get_width()) // 2, 240))
+        for i, opt in enumerate(menu_options):
+            txt = font.render(f"> {opt} <" if i == current_menu_index else opt, True, NEON_ORANGE if i == current_menu_index else WHITE)
+            screen.blit(txt, ((WIDTH - txt.get_width()) // 2, 360 + i * 55))
+        inf = music_font.render("Use UP / DOWN arrows and ENTER to select", True, GRAY)
+        screen.blit(inf, ((WIDTH - inf.get_width()) // 2, 540))
+    elif game_state == "MUSIC_SELECT":
+        t_text = font.render("SELECT BACKGROUND MUSIC", True, NEON_CYAN)
+        screen.blit(t_text, ((WIDTH - t_text.get_width()) // 2, 60))
+        for i, trk in enumerate(tracks):
+            txt = font.render(f"[{i+1}] {trk['name']} (Active)" if i == current_track_index else f"[{i+1}] {trk['name']}", True, NEON_ORANGE if i == current_track_index else WHITE)
+            screen.blit(txt, ((WIDTH - txt.get_width()) // 2, 160 + i * 55))
+        back = music_font.render("Press 1, 2, 3, 4 to listen. Press ESC / ENTER to return to Menu", True, YELLOW)
+        screen.blit(back, ((WIDTH - back.get_width()) // 2, 450))
+    elif game_state == "PLAY":
+        if not game_over and not win:
+            if move_left and paddle.x > 0: paddle.x -= 8
+            if move_right and paddle.x < WIDTH - 120: paddle.x += 8
+            ball.x += ball_dx
+            ball.y += ball_dy
+            if ball.x <= 0 or ball.x >= WIDTH - 16: ball_dx = -ball_dx
+            if ball.y <= 0: ball_dy = -ball_dy
+            if ball.y >= HEIGHT:
+                game_over = True
+                pygame.mixer.music.stop()
+                game_over_sound.play()
+            if ball.colliderect(paddle): ball_dy = -ball_dy
+            for brk in bricks:
+                if ball.colliderect(brk):
+                    bricks.remove(brk)
+                    ball_dy = -ball_dy
+                    score += 10
+                    break
+            if not bricks:
+                win = True
+                pygame.mixer.music.stop()
+                win_sound.play()
+            pygame.draw.rect(screen, NEON_CYAN, paddle)
+            pygame.draw.circle(screen, WHITE, ball.center, 8)
+            for brk in bricks: pygame.draw.rect(screen, NEON_ORANGE, brk)
+            screen.blit(font.render(f"Score: {score}", True, YELLOW), (20, 15))
+        if game_over or win:
+            f_txt = final_font.render("GAME OVER" if game_over else "YOU WIN", True, RED if game_over else GREEN)
+            screen.blit(f_txt, ((WIDTH - f_txt.get_width()) // 2, 160))
+            r_txt = retry_font.render("Press ENTER to return to Menu", True, WHITE)
+            screen.blit(r_txt, ((WIDTH - r_txt.get_width()) // 2, 380))
+            m_txt = music_font.render(f"Current Music: {tracks[current_track_index]['name']}", True, GRAY)
+            screen.blit(m_txt, ((WIDTH - m_txt.get_width()) // 2, 450))
+            h_txt = music_font.render("Press 1, 2, 3, 4 to change music track", True, YELLOW)
+            screen.blit(h_txt, ((WIDTH - h_txt.get_width()) // 2, 490))
     pygame.display.flip()
     clock.tick(FPS)
-
 pygame.quit()
